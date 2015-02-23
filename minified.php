763c3f00afa9a88be074605b24001252b80786d3
Pablo Munoz <pjmd89@gmail.com>
2015-02-23 12:59:10 -0430

    Changes to be committed: 	new file:   css/alerts.css 	new file:   css/base.css 	new file:   css/buttons.css 	new file:   css/containers.css 	new file:   css/debugger.css 	new file:   css/display-table.css 	new file:   css/fileupload.css 	new file:   css/forms.css 	new file:   css/icons-16.css 	new file:   css/icons.css 	new file:   css/modal.css 	new file:   css/navbars.css 	new file:   css/sct.min.css 	new file:   css/table.css 	new file:   css/tooltips.css 	new file:   img/sct-icons-16.png 	new file:   img/sct-icons-32.png 	new file:   index.html 	new file:   minified.php

---
+++ minified.php
@@ -0,0 +1,58 @@
+<?php 
+$files = array(
+	"alerts.css",
+	"base.css",
+	"buttons.css",
+	"containers.css",
+	"forms.css",
+	"navbars.css",
+	"tooltips.css",
+	"display-table.css",
+	"icons.css",
+	"icons-16.css",
+	"fileupload.css",
+	"modal.css",
+	"table.css",
+	"debugger.css"
+);
+
+$path = "css";
+
+$out = "sct.min.css";
+
+minified($files, $path, $out);
+
+function minified( $files , $path , $out ){
+	
+	$scan = scandir( $path );
+	
+	$fileToMini = array();
+	
+	if( count( $scan ) > 0 ){
+		
+		$mini = "";
+		
+		foreach( $scan as $file ){
+			
+			if( in_array($file , $files ) ){
+				
+				$openFile = file_get_contents($path."/".$file);
+				
+				$openFile =preg_replace('/\/\*[^\*]+\*\//', '', $openFile);
+				$openFile =preg_replace('/,[\n\t\s]+/', ',', $openFile);
+				$openFile =preg_replace('/{[\n\t\s]+/', '{', $openFile);
+				$openFile =preg_replace('/[\n\t\s]+{/', '{', $openFile);
+				$openFile =preg_replace('/;[\n\t\s]+/', ';', $openFile);
+				$openFile =preg_replace('/}[\n\t\s]+/', '}', $openFile);
+				
+				$mini.=$openFile;
+			}
+		}
+		$handle = fopen($path."/".$out, "w");
+		
+		fwrite( $handle , $mini );
+		
+		fclose( $handle );
+	}
+}
+?>
